11e710edae811b55f17de701e398eee3ae612799
Mr.01010011 <3061928404@qq.com>
2023-01-08 23:37:55 +0800

    [修复]修复因特殊符号而导致项目无法启动

--- src/main/java/cn/siriusbot/siriuspro/webapi/controller/api/ChannelPermissionsApiController.java
+++ src/main/java/cn/siriusbot/siriuspro/webapi/controller/api/ChannelPermissionsApiController.java
@@ -17,6 +17,7 @@ public class ChannelPermissionsApiController {
 
     @Autowired
     ChannelPermissionsApi channelPermissionsApi;
+
     /**
      * 获取子频道用户权限
      * 用于获取 子频道channel_id 下用户 user_id 的权限。
@@ -32,14 +33,12 @@ public class ChannelPermissionsApiController {
     public R getChannelUserPermissions(@PathVariable String bot_id, @PathVariable String channel_id, @PathVariable String user_id) {
         try {
             return new R().setData(channelPermissionsApi.getChannelPermissionsByUser_id(bot_id, channel_id, user_id));
-        }catch (MsgException e){
+        } catch (MsgException e) {
             return e.getR();
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             return new R().setCode(500).setMsg("error");
         }
     }
-
 
 
     /**
@@ -48,7 +47,7 @@ public class ChannelPermissionsApiController {
      * 获取子频道用户权限。
      * 要求操作人具有管理子频道的权限，如果是机器人，则需要将机器人设置为管理员
      *
-     * @param bot_id        传入机器人ID
+     * @param bot_id     传入机器人ID
      * @param channel_id 子频道ID
      * @param user_id    用户id
      * @param add        添加的权限
@@ -56,14 +55,12 @@ public class ChannelPermissionsApiController {
      * @return 返回修改结果
      */
     @PutMapping("/modify-user-channel-permissions/{bot_id}/{channel_id}/user_id/{add}/{remove}")
-    public R modifyUserChannelPermissions (@PathVariable String bot_id, @PathVariable String channel_id, @PathVariable String user_id,@PathVariable String add,@PathVariable String remove){
-        try{
+    public R modifyUserChannelPermissions(@PathVariable String bot_id, @PathVariable String channel_id, @PathVariable String user_id, @PathVariable String add, @PathVariable String remove) {
+        try {
             return new R().setData(channelPermissionsApi.modifyChannelPermissionsByUser_id(bot_id, channel_id, user_id, add, remove));
-        }
-        catch (MsgException e){
+        } catch (MsgException e) {
             return e.getR();
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             return new R().setCode(500).setMsg("error");
         }
     }
@@ -74,20 +71,18 @@ public class ChannelPermissionsApiController {
      * 用于获取子频道 channel_id 下身份组 role_id 的权限。
      * 要求操作人具有管理子频道的权限，如果是机器人，则需要将机器人设置为管理员。
      *
-     * @param bot_id        传入机器人ID
+     * @param bot_id     传入机器人ID
      * @param channel_id 子频道ID
      * @param role_id    身份组ID
      * @return 子频道权限对象
      */
     @GetMapping("/get-channel-role-permissions/{bot_id}/{channel_id}/{role_id}")
-    public R getChannelRolePermissions(@PathVariable String bot_id,@PathVariable String channel_id,@PathVariable String role_id){
+    public R getChannelRolePermissions(@PathVariable String bot_id, @PathVariable String channel_id, @PathVariable String role_id) {
         try {
             return new R().setData(channelPermissionsApi.getChannelPermissionsByRole_id(bot_id, channel_id, role_id));
-        }
-        catch (MsgException e){
-            return  e.getR();
-        }
-        catch (Exception e){
+        } catch (MsgException e) {
+            return e.getR();
+        } catch (Exception e) {
             return new R().setCode(500).setMsg("error");
         }
     }
@@ -98,7 +93,7 @@ public class ChannelPermissionsApiController {
      * 获取子频道用户权限。
      * 要求操作人具有管理子频道的权限，如果是机器人，则需要将机器人设置为管理员
      *
-     * @param bot_id        传入机器人ID
+     * @param bot_id     传入机器人ID
      * @param channel_id 子频道ID
      * @param role_id    身份组ID
      * @param add        要添加的权限
@@ -106,15 +101,12 @@ public class ChannelPermissionsApiController {
      * @return 修改结果
      */
     @PutMapping("/modify-role-channel-permissions/{bot_id}/{channel_id}/{role_id}/{add}/{remove}")
-    public R modifyRoleChannelPermissions(@PathVariable String bot_id,@PathVariable String channel_id,@PathVariable String role_id,@PathVariable String add,@PathVariable String remove){
-        try
-        {
+    public R modifyRoleChannelPermissions(@PathVariable String bot_id, @PathVariable String channel_id, @PathVariable String role_id, @PathVariable String add, @PathVariable String remove) {
+        try {
             return new R().setData(channelPermissionsApi.modifyChannelPermissionsByRole_id(bot_id, channel_id, role_id, add, remove));
-        }
-        catch (MsgException e){
+        } catch (MsgException e) {
             return e.getR();
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             return new R().setMsg("error").setCode(500);
         }
     }
--- src/main/java/cn/siriusbot/siriuspro/webapi/controller/api/MemberApiController.java
+++ src/main/java/cn/siriusbot/siriuspro/webapi/controller/api/MemberApiController.java
@@ -24,76 +24,79 @@ public class MemberApiController {
 
     /**
      * 获取频道成员列表
-     * @param bot_id 传入机器人ID
-     * @param guild_id 频道ID
-     * @param after 上一次回包中最后一个member的user id， 如果是第一次请求填 0，默认为 0
-     * @return 返回成员列表 分页大小，1-400，默认是 1。成员较多的频道尽量使用较大的limit值，以减少请求数
      *
+     * @param bot_id   传入机器人ID
+     * @param guild_id 频道ID
+     * @param after    上一次回包中最后一个member的user id， 如果是第一次请求填 0，默认为 0
+     * @return 返回成员列表 分页大小，1-400，默认是 1。成员较多的频道尽量使用较大的limit值，以减少请求数
      */
     @GetMapping("/get-members/{bot_id}/{guild_id}/{after}/{limit}")
-    public R getMemberList(@PathVariable String bot_id, @PathVariable String guild_id, @PathVariable @Nullable String after, @PathVariable @Nullable Integer limit){
-        try{
-            return new R().setData(memberApi.getMemberList(bot_id,guild_id,after,limit));
-        }catch (MsgException e){
+    public R getMemberList(@PathVariable String bot_id, @PathVariable String guild_id, @PathVariable @Nullable String after, @PathVariable @Nullable Integer limit) {
+        try {
+            return new R().setData(memberApi.getMemberList(bot_id, guild_id, after, limit));
+        } catch (MsgException e) {
             return e.getR();
-        }catch (Exception e){
-            return new R().setMsg("error").setCode(500);
-        }
-    }
-    /**
-     * 获取成员详情
-     * @param bot_id 传入机器人ID
-     * @param guild_id 频道ID
-     * @param user_id 用户ID
-     * @return 返回成员对象
-     */
-    @GetMapping("/get-member-info/{bot_id}/{guild_id}/{user_id}")
-    public R getMemberInfo(@PathVariable String bot_id,@PathVariable String guild_id,@PathVariable String user_id){
-        try{
-            return new R().setData(memberApi.getMemberInfo(bot_id, guild_id, user_id));
-        }catch (MsgException e){
-            return e.getR();
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             return new R().setMsg("error").setCode(500);
         }
     }
 
-`    /**
-     * 获取拥有此身份组的成员列表
-     * @param bot_id 传入机器人ID
+    /**
+     * 获取成员详情
+     *
+     * @param bot_id   传入机器人ID
      * @param guild_id 频道ID
-     * @param role_id 身份组ID
+     * @param user_id  用户ID
+     * @return 返回成员对象
+     */
+    @GetMapping("/get-member-info/{bot_id}/{guild_id}/{user_id}")
+    public R getMemberInfo(@PathVariable String bot_id, @PathVariable String guild_id, @PathVariable String user_id) {
+        try {
+            return new R().setData(memberApi.getMemberInfo(bot_id, guild_id, user_id));
+        } catch (MsgException e) {
+            return e.getR();
+        } catch (Exception e) {
+            return new R().setMsg("error").setCode(500);
+        }
+    }
+
+    /**
+     * 获取拥有此身份组的成员列表
+     *
+     * @param bot_id      传入机器人ID
+     * @param guild_id    频道ID
+     * @param role_id     身份组ID
      * @param start_index 上一次返回包中的next，第一次请求填0，默认0
      * @return 返回持有指定身份组ID的成员列表
      */
     @GetMapping("/get-role-member/{bot_id}/guild_id/{role_id}/{start_index}/{limit}")
-    public R getMemberListByRoleId(@PathVariable String bot_id,@PathVariable String guild_id,@PathVariable String role_id,@Nullable @PathVariable String start_index, @Nullable @PathVariable Integer limit){
-        try{
-            return new R().setData(memberApi.getMemberListByRoleId(bot_id,guild_id,role_id,start_index,limit));
-        }catch (MsgException e){
+    public R getMemberListByRoleId(@PathVariable String bot_id, @PathVariable String guild_id, @PathVariable String role_id, @Nullable @PathVariable String start_index, @Nullable @PathVariable Integer limit) {
+        try {
+            return new R().setData(memberApi.getMemberListByRoleId(bot_id, guild_id, role_id, start_index, limit));
+        } catch (MsgException e) {
             return e.getR();
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             return new R().setMsg("error").setCode(500);
         }
     }
+
     /**
      * 将指定成员从频道内移除
-     * @param bot_id 传入机器人ID
-     * @param user_id 用户ID
-     * @param guild_id 频道ID
-     * @param add_black 添加到黑名单
+     *
+     * @param bot_id                  传入机器人ID
+     * @param user_id                 用户ID
+     * @param guild_id                频道ID
+     * @param add_black               添加到黑名单
      * @param delete_history_msg_days 撤回消息的天数
      * @return 移除结果
      */
     @DeleteMapping("/delete-member/{bot_id}/{guild_id}/{user_id}")
-    public R deleteMemberByUserId(@PathVariable String bot_id,@PathVariable String guild_id,@PathVariable String user_id,@RequestParam Boolean add_black,@RequestParam Integer delete_history_msg_days){
-        try{
-            return new R().setData(memberApi.deleteMemberByUserId(bot_id,guild_id,user_id,add_black,delete_history_msg_days));
-        }catch (MsgException e){
+    public R deleteMemberByUserId(@PathVariable String bot_id, @PathVariable String guild_id, @PathVariable String user_id, @RequestParam Boolean add_black, @RequestParam Integer delete_history_msg_days) {
+        try {
+            return new R().setData(memberApi.deleteMemberByUserId(bot_id, guild_id, user_id, add_black, delete_history_msg_days));
+        } catch (MsgException e) {
             return e.getR();
-        }catch (Exception e){
+        } catch (Exception e) {
             return new R().setMsg("error").setCode(500);
         }
 
