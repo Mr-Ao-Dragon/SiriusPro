bf2d369649c42557d97d856a2e3b85b0a875db0e
Mr.01010011 <37700432+SichuanMirrorCore@users.noreply.github.com>
2023-01-15 22:45:07 +0800

    [优化]禁言Api

--- src/main/java/cn/siriusbot/siriuspro/bot/api/impl/NoSpeakImpl.java
+++ src/main/java/cn/siriusbot/siriuspro/bot/api/impl/NoSpeakImpl.java
@@ -5,6 +5,12 @@ import cn.siriusbot.siriuspro.bot.SiriusBotClient;
 import cn.siriusbot.siriuspro.bot.api.NoSpeakApi;
 import cn.siriusbot.siriuspro.bot.api.pojo.NoSpeak;
 import cn.siriusbot.siriuspro.bot.api.tuple.Tuple;
+import cn.siriusbot.siriuspro.bot.client.BotClient;
+import cn.siriusbot.siriuspro.bot.event.BotHttpEvent;
+import cn.siriusbot.siriuspro.bot.pojo.BotRequest;
+import cn.siriusbot.siriuspro.bot.pojo.BotResponse;
+import cn.siriusbot.siriuspro.bot.pojo.e.RequestMethod;
+import cn.siriusbot.siriuspro.config.bean.BotPool;
 import cn.siriusbot.siriuspro.http.SiriusHttpUtils;
 import com.alibaba.fastjson.JSONObject;
 import lombok.SneakyThrows;
@@ -16,58 +22,64 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+
 @Component
-public class  NoSpeakImpl implements NoSpeakApi {
+public class NoSpeakImpl implements NoSpeakApi {
 
     @Autowired
     BotManager botManager;
 
+    @Autowired
+    BotPool botPool;
+
     /**
      * 禁言指定成员
-     * @param bot_id 传入机器人对象ID
-     * @param guild_id 频道ID
-     * @param user_id 用户ID
+     *
+     * @param bot_id             传入机器人对象ID
+     * @param guild_id           频道ID
+     * @param user_id            用户ID
      * @param mute_end_timestamp 禁言到期时间戳
-     * @param mute_seconds 禁言秒数
+     * @param mute_seconds       禁言秒数
      * @return 返回禁言结果
      */
     @Override
     public Boolean noSpeakByUser_id(String bot_id, String guild_id, String user_id, String mute_end_timestamp, String mute_seconds) {
-        SiriusBotClient siriusBotClient = botManager.getBotByBotId(bot_id);
-        Request request = new Request.Builder().url(siriusBotClient.getSocket().getOpenUrl() + "guilds/" + guild_id + "/members/" + user_id + "/mute").build();
-        JSONObject json = new JSONObject();
-        json.put("mute_end_timestamp", mute_end_timestamp);
-        json.put("mute_seconds", mute_seconds);
-        MediaType mediaType = MediaType.parse("application/json;text/plain");
-        RequestBody body = RequestBody.create(mediaType, json.toJSONString());
-        Response response = SiriusHttpUtils.patchRequest(siriusBotClient, request, body);
-        return response.code() == 204;
+        BotClient client = botPool.getBotById(bot_id);
+        BotRequest botRequest = new BotRequest()
+                .setUrl(client.getSession().getOpenUrl() + "guilds/" + guild_id + "/members/" + user_id + "/mute")
+                .setMethod(RequestMethod.PATCH)
+                .putRequestBody("mute_end_timestamp", mute_end_timestamp)
+                .putRequestBody("mute_seconds", mute_seconds);
+        BotHttpEvent http = client.getBean(BotHttpEvent.class);
+        BotResponse response = http.req(botRequest);
+        return response.getCode() == 204;
     }
 
 
     /**
      * 批量禁言成员
-     * @param bot_id 传入机器人对象ID
-     * @param guild_id 频道ID
-     * @param user_ids 需要禁言的成员列表
+     *
+     * @param bot_id             传入机器人对象ID
+     * @param guild_id           频道ID
+     * @param user_ids           需要禁言的成员列表
      * @param mute_end_timestamp 禁言到期时间戳
-     * @param mute_seconds 禁言秒数
+     * @param mute_seconds       禁言秒数
      * @return 返回禁言成员对象
      */
     @SneakyThrows
     @Override
-    public Tuple<NoSpeak,String> noSpeakByUser_ids(String bot_id, String guild_id, List<String> user_ids, String mute_end_timestamp, String mute_seconds) {
-        SiriusBotClient siriusBotClient = botManager.getBotByBotId(bot_id);
-        Request request = new Request.Builder().url(siriusBotClient.getSocket().getOpenUrl() + "guilds/" + guild_id + "/mute").build();
-        JSONObject json = new JSONObject();
-        MediaType mediaType = MediaType.parse("application/json;text/plain");
-        json.put("mute_end_timestamp", mute_end_timestamp);
-        json.put("mute_seconds", mute_seconds);
-        json.put("user_ids", user_ids);
-        RequestBody body = RequestBody.create(mediaType, json.toJSONString());
-        Response response = SiriusHttpUtils.patchRequest(siriusBotClient, request, body);
-        String data = response.body().string();
-        Tuple<NoSpeak,String> tuple = new Tuple<>();
+    public Tuple<NoSpeak, String> noSpeakByUser_ids(String bot_id, String guild_id, List<String> user_ids, String mute_end_timestamp, String mute_seconds) {
+        BotClient client = botPool.getBotById(bot_id);
+        BotRequest botRequest = new BotRequest()
+                .setMethod(RequestMethod.PATCH)
+                .setUrl(client.getSession().getOpenUrl() + "guilds/" + guild_id + "/mute")
+                .putRequestBody("mute_end_timestamp", mute_end_timestamp)
+                .putRequestBody("mute_seconds", mute_seconds)
+                .putRequestBody("user_ids", user_ids);
+        BotHttpEvent http = client.getBean(BotHttpEvent.class);
+        BotResponse response = http.req(botRequest);
+        String data = response.getBody();
+        Tuple<NoSpeak, String> tuple = new Tuple<>();
         tuple.setFirst(JSONObject.parseObject(data, NoSpeak.class)).setSecond(data);
         return tuple;
     }
@@ -75,21 +87,22 @@ public class  NoSpeakImpl implements NoSpeakApi {
 
     /**
      * 全员禁言
-     * @param bot_id 传入机器人对象ID
+     *
+     * @param bot_id             传入机器人对象ID
      * @param mute_end_timestamp 禁言到期时间戳
-     * @param mute_seconds 禁言秒数
+     * @param mute_seconds       禁言秒数
      * @return 返回禁言结果
      */
     @Override
     public Boolean nodeSpeakAll(String bot_id, String guild_id, String mute_end_timestamp, String mute_seconds) {
-        SiriusBotClient siriusBotClient = botManager.getBotByBotId(bot_id);
-        MediaType mediaType = MediaType.parse("application/json;text/plain");
-        Request request = new Request.Builder().url(siriusBotClient.getSocket().getOpenUrl() + "guilds/" + guild_id + "/mute").build();
-        JSONObject json = new JSONObject();
-        json.put("mute_end_timestamp", mute_end_timestamp);
-        json.put("mute_seconds", mute_seconds);
-        RequestBody body = RequestBody.create(mediaType, json.toJSONString());
-        Response response = SiriusHttpUtils.patchRequest(siriusBotClient, request, body);
-        return response.code() == 204;
+        BotClient client = botPool.getBotById(bot_id);
+        BotRequest botRequest = new BotRequest()
+                .setUrl(client.getSession().getOpenUrl() + "guilds/" + guild_id + "/mute")
+                .setMethod(RequestMethod.PATCH)
+                .putRequestBody("mute_end_timestamp", mute_end_timestamp)
+                .putRequestBody("mute_seconds", mute_seconds);
+        BotHttpEvent http = client.getBean(BotHttpEvent.class);
+        BotResponse response = http.req(botRequest);
+        return response.getCode() == 204;
     }
 }
