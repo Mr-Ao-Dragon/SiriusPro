634ea5dc65019528625f356619c8ae1f0aa8c572
Mr.01010011 <37700432+SichuanMirrorCore@users.noreply.github.com>
2023-01-15 23:36:40 +0800

    [优化]精华消息Api

--- src/main/java/cn/siriusbot/siriuspro/bot/api/impl/PinsMessageImpl.java
+++ src/main/java/cn/siriusbot/siriuspro/bot/api/impl/PinsMessageImpl.java
@@ -5,6 +5,12 @@ import cn.siriusbot.siriuspro.bot.SiriusBotClient;
 import cn.siriusbot.siriuspro.bot.api.PinsMessageApi;
 import cn.siriusbot.siriuspro.bot.api.pojo.PinsMessage;
 import cn.siriusbot.siriuspro.bot.api.tuple.Tuple;
+import cn.siriusbot.siriuspro.bot.client.BotClient;
+import cn.siriusbot.siriuspro.bot.event.BotHttpEvent;
+import cn.siriusbot.siriuspro.bot.pojo.BotRequest;
+import cn.siriusbot.siriuspro.bot.pojo.BotResponse;
+import cn.siriusbot.siriuspro.bot.pojo.e.RequestMethod;
+import cn.siriusbot.siriuspro.config.bean.BotPool;
 import cn.siriusbot.siriuspro.http.SiriusHttpUtils;
 import com.alibaba.fastjson.JSONObject;
 import com.vdurmont.emoji.EmojiParser;
@@ -15,16 +21,21 @@ import okhttp3.RequestBody;
 import okhttp3.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
 @Component
-public class  PinsMessageImpl implements PinsMessageApi {
+public class PinsMessageImpl implements PinsMessageApi {
 
     @Autowired
     BotManager botManager;
-    
+
+
+    @Autowired
+    BotPool botPool;
+
     /**
      * 添加精华消息
      *
-     * @param bot_id        传入机器人对象ID
+     * @param bot_id     传入机器人对象ID
      * @param channel_id 子频道ID
      * @param message_id 消息ID
      * @return 返回精华消息对象
@@ -32,13 +43,13 @@ public class  PinsMessageImpl implements PinsMessageApi {
     @SneakyThrows
     @Override
     public Tuple<PinsMessage, String> addPinsMessage(String bot_id, String channel_id, String message_id) {
-        SiriusBotClient siriusBotClient = botManager.getBotByBotId(bot_id);
-        Request request = new Request.Builder().url(siriusBotClient.getSocket().getOpenUrl() + "channels/" + channel_id + "/pins/" + message_id).build();
-        MediaType mediaType = MediaType.parse("application/json;text/plain");
-        RequestBody body = RequestBody.create(mediaType, "");
-        Response response = SiriusHttpUtils.putRequest(siriusBotClient, request, body);
-        String data = response.body().string();
-        data = EmojiParser.parseToUnicode(data);
+        BotClient client = botPool.getBotById(bot_id);
+        BotRequest botRequest = new BotRequest()
+                .setUrl("channels/" + channel_id + "/pins/" + message_id)
+                .setMethod(RequestMethod.PUT);
+        BotHttpEvent http = client.getBean(BotHttpEvent.class);
+        BotResponse response = http.req(botRequest);
+        String data = EmojiParser.parseToUnicode(response.getBody());
         PinsMessage pinsMessage = JSONObject.parseObject(data, PinsMessage.class);
         Tuple<PinsMessage, String> tuple = new Tuple<>();
         tuple.setFirst(pinsMessage).setSecond(data);
@@ -49,19 +60,22 @@ public class  PinsMessageImpl implements PinsMessageApi {
     /**
      * 获取当前子频道精华消息
      *
-     * @param bot_id        传入机器人对象ID
+     * @param bot_id     传入机器人对象ID
      * @param channel_id 子频道ID
      * @return 返回精华消息对象
      */
     @SneakyThrows
     @Override
-    public Tuple<PinsMessage,String> getPinsMessage(String bot_id, String channel_id) {
-        SiriusBotClient siriusBotClient = botManager.getBotByBotId(bot_id);
-        Request request = new Request.Builder().url(siriusBotClient.getSocket().getOpenUrl() + "channels/" + channel_id + "/pins").build();
-        String data = SiriusHttpUtils.getRequest(siriusBotClient, request).body().string();
-        data = EmojiParser.parseToUnicode(data);
-        PinsMessage pinsMessage = JSONObject.parseObject(SiriusHttpUtils.getRequest(siriusBotClient, request).body().string(), PinsMessage.class);
-        Tuple<PinsMessage,String> tuple = new Tuple<>();
+    public Tuple<PinsMessage, String> getPinsMessage(String bot_id, String channel_id) {
+        BotClient client = botPool.getBotById(bot_id);
+        BotRequest botRequest = new BotRequest()
+                .setMethod(RequestMethod.GET)
+                .setUrl(client.getSession().getOpenUrl() + "channels/" + channel_id + "/pins");
+        BotHttpEvent http = client.getBean(BotHttpEvent.class);
+        BotResponse response = http.req(botRequest);
+        String data = EmojiParser.parseToUnicode(response.getBody());
+        PinsMessage pinsMessage = JSONObject.parseObject(data, PinsMessage.class);
+        Tuple<PinsMessage, String> tuple = new Tuple<>();
         tuple.setFirst(pinsMessage).setSecond(data);
         return tuple;
     }
@@ -70,15 +84,19 @@ public class  PinsMessageImpl implements PinsMessageApi {
     /**
      * 删除精华消息
      *
-     * @param bot_id        传入机器人对象ID
+     * @param bot_id     传入机器人对象ID
      * @param channel_id 子频道ID
      * @param message_id 消息ID
      * @return 删除结果
      */
     @Override
     public Boolean deletePinsMessage(String bot_id, String channel_id, String message_id) {
-        SiriusBotClient siriusBotClient = botManager.getBotByBotId(bot_id);
-        Request request = new Request.Builder().url(siriusBotClient.getSocket().getOpenUrl() + "channels/" + channel_id + "/pins/" + message_id).build();
-        return SiriusHttpUtils.deleteRequest(siriusBotClient, request, null).code() == 204;
+        BotClient client = botPool.getBotById(bot_id);
+        BotRequest botRequest = new BotRequest()
+                .setUrl(client.getSession().getOpenUrl() + "channels/" + channel_id + "/pins/" + message_id)
+                .setMethod(RequestMethod.DELETE);
+        BotHttpEvent http = client.getBean(BotHttpEvent.class);
+        BotResponse response = http.req(botRequest);
+        return response.getCode() == 204;
     }
 }
