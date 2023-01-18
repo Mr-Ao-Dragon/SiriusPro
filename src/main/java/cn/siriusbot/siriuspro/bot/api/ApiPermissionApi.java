43c441e6b6491f9c500d6d5b6a229f3f352c543d
Mr.01010011 <37700432+SichuanMirrorCore@users.noreply.github.com>
2023-01-18 16:52:49 +0800

    [新增]api权限Api参数限制

--- src/main/java/cn/siriusbot/siriuspro/bot/api/ApiPermissionApi.java
+++ src/main/java/cn/siriusbot/siriuspro/bot/api/ApiPermissionApi.java
@@ -1,6 +1,7 @@
 package cn.siriusbot.siriuspro.bot.api;
 
 
+import cn.siriusbot.siriuspro.bot.annotation.EDoc;
 import cn.siriusbot.siriuspro.bot.annotation.ENonNull;
 import cn.siriusbot.siriuspro.bot.api.pojo.apipermission.APIPermission;
 import cn.siriusbot.siriuspro.bot.api.pojo.apipermission.ApiPermissionDemand;
@@ -17,20 +18,45 @@ public interface ApiPermissionApi {
 
     /**
      * 创建频道Api接口权限，授权链接
-     * @param bot_id 传入机器人ID
-     * @param guild_id 频道ID
-     * @param channel_id 子频道ID
+     *
+     * @param bot_id       传入机器人ID
+     * @param guild_id     频道ID
+     * @param channel_id   子频道ID
      * @param api_identify Api权限需求标识对象
-     * @param desc 机器人申请对于的API接口权限后，可使用功能的描述
+     * @param desc         机器人申请对于的API接口权限后，可使用功能的描述
      * @return Api接口权限需求对象
      */
-    public abstract Tuple<ApiPermissionDemand,String> createApiGrantLink(@NonNull @ENonNull String bot_id, String guild_id, String channel_id, ApiPermissionDemandIdentify api_identify, String desc);
+    Tuple<ApiPermissionDemand, String> createApiGrantLink
+    (
+            @EDoc(doc = "机器人ID")
+            @NonNull @ENonNull String bot_id,
+
+            @EDoc(doc = "频道ID")
+            @NonNull @ENonNull String guild_id,
+
+            @EDoc(doc = "子频道ID")
+            @NonNull @ENonNull String channel_id,
+
+            @EDoc(doc = "api权限对象")
+            @NonNull @ENonNull ApiPermissionDemandIdentify api_identify,
+
+            @EDoc(doc = "描述")
+            @NonNull @ENonNull String desc
+    );
 
     /**
      * 获取频道可用权限列表
-     * @param bot_id 传入机器人ID
+     *
+     * @param bot_id   传入机器人ID
      * @param guild_id 频道ID
      * @return 返回可用Api权限对象列表
      */
-    public abstract Tuple<List<APIPermission>,String> getAPIPermissions(@NonNull @ENonNull String bot_id, String guild_id);
+    Tuple<List<APIPermission>, String> getAPIPermissions
+    (
+            @EDoc(doc = "机器人ID")
+            @NonNull @ENonNull String bot_id,
+
+            @EDoc(doc = "频道ID")
+            @NonNull @ENonNull String guild_id
+    );
 }
