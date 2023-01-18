fd403dbaef0e5d374294c28c0e2f5aa75d7d1bae
Mr.01010011 <37700432+SichuanMirrorCore@users.noreply.github.com>
2023-01-18 21:17:33 +0800

    [新增]日程Api参数限制

--- src/main/java/cn/siriusbot/siriuspro/bot/api/impl/ScheduleApiImpl.java
+++ src/main/java/cn/siriusbot/siriuspro/bot/api/impl/ScheduleApiImpl.java
@@ -36,7 +36,7 @@ public class ScheduleApiImpl implements ScheduleApi {
      */
     @SneakyThrows
     @Override
-    public Tuple<List<Schedule>, String> getScheduleListByChannel_id(@NotNull String bot_id, String channel_id, String since) {
+    public Tuple<List<Schedule>, String> getScheduleListByChannel_id(@NotNull String bot_id, @NotNull String channel_id, String since) {
         BotClient client = botPool.getBotById(bot_id);
         BotRequest botRequest = new BotRequest()
                 .setMethod(RequestMethod.GET)
@@ -62,7 +62,7 @@ public class ScheduleApiImpl implements ScheduleApi {
      */
     @SneakyThrows
     @Override
-    public Tuple<Schedule, String> getScheduleInfo(@NotNull String bot_id, String channel_id, String schedule_id) {
+    public Tuple<Schedule, String> getScheduleInfo(@NotNull String bot_id, @NotNull String channel_id, @NotNull String schedule_id) {
         BotClient client = botPool.getBotById(bot_id);
         BotRequest botRequest = new BotRequest()
                 .setUrl(client.getSession().getOpenUrl() + "channels/" + channel_id + "schedules/" + schedule_id)
@@ -86,7 +86,7 @@ public class ScheduleApiImpl implements ScheduleApi {
      * @return 返回操作结果
      */
     @Override
-    public Boolean deleteSchedule(@NotNull String bot_id, String channel_id, String schedule_id) {
+    public Boolean deleteSchedule(@NotNull String bot_id, @NotNull String channel_id, @NotNull String schedule_id) {
         BotClient client = botPool.getBotById(bot_id);
         BotRequest botRequest = new BotRequest()
                 .setMethod(RequestMethod.DELETE)
@@ -107,7 +107,7 @@ public class ScheduleApiImpl implements ScheduleApi {
      */
     @SneakyThrows
     @Override
-    public Tuple<Schedule, String> createSchedule(@NotNull String bot_id, String channel_id, Schedule schedule) {
+    public Tuple<Schedule, String> createSchedule(@NotNull String bot_id, @NotNull String channel_id, @NotNull Schedule schedule) {
         BotClient client = botPool.getBotById(bot_id);
         BotRequest botRequest = new BotRequest()
                 .setUrl("channels/" + channel_id + "/schedules")
@@ -134,7 +134,7 @@ public class ScheduleApiImpl implements ScheduleApi {
      */
     @SneakyThrows
     @Override
-    public Tuple<Schedule, String> modifySchedule(@NotNull String bot_id, String channel_id, String schedule_id, Schedule schedule) {
+    public Tuple<Schedule, String> modifySchedule(@NotNull String bot_id, @NotNull String channel_id, @NotNull String schedule_id, @NotNull Schedule schedule) {
         BotClient client = botPool.getBotById(bot_id);
         BotRequest botRequest = new BotRequest()
                 .setMethod(RequestMethod.PATCH)
