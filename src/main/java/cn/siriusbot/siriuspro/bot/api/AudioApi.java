431c79460d2e3f480e763d38276c42b7b5a14126
Mr.01010011 <37700432+SichuanMirrorCore@users.noreply.github.com>
2023-01-18 16:55:30 +0800

    [新增]音频控制Api参数限制

--- src/main/java/cn/siriusbot/siriuspro/bot/api/AudioApi.java
+++ src/main/java/cn/siriusbot/siriuspro/bot/api/AudioApi.java
@@ -1,5 +1,6 @@
 package cn.siriusbot.siriuspro.bot.api;
 
+import cn.siriusbot.siriuspro.bot.annotation.EDoc;
 import cn.siriusbot.siriuspro.bot.annotation.ENonNull;
 import cn.siriusbot.siriuspro.bot.api.pojo.audio.AudioControl;
 import lombok.NonNull;
@@ -13,26 +14,54 @@ public interface AudioApi {
      * 音频控制 Api
      * 频接口：仅限音频类机器人才能使用，后续会根据机器人类型自动开通接口权限，现如需调用，需联系平台申请权限。
      *
-     * @param bot_id          机器人对象
+     * @param bot_id       机器人对象
      * @param channel_id   子频道ID
      * @param audioControl 音频控制对象
      * @return 操作结果
      */
-    public abstract Boolean audioControl(@NonNull @ENonNull String bot_id, String channel_id, AudioControl audioControl);
+    Boolean audioControl
+    (
+            @EDoc(doc = "机器人ID")
+            @NonNull @ENonNull String bot_id,
+
+            @EDoc(doc = "子频道ID")
+            @NonNull @ENonNull
+            String channel_id,
+
+            @EDoc(doc = "音频控制对象")
+            @NonNull @ENonNull
+            AudioControl audioControl
+    );
 
     /**
      * 机器人上麦
-     * @param bot_id 传入机器人ID
+     *
+     * @param bot_id     传入机器人ID
      * @param channel_id 子频道ID
      * @return 返回操作结果
      */
-    public abstract Boolean singStart(@NonNull @ENonNull String bot_id, String channel_id);
+    Boolean singStart
+    (
+            @EDoc(doc = "机器人ID")
+            @NonNull @ENonNull String bot_id,
+
+            @EDoc(doc = "子频道ID")
+            @NonNull @ENonNull String channel_id
+    );
 
     /**
      * 机器人下麦
-     * @param bot_id 传入机器人ID
+     *
+     * @param bot_id     传入机器人ID
      * @param channel_id 子频道ID
      * @return 操作结果
      */
-    public abstract  Boolean singEnd(@NonNull @ENonNull String bot_id,String channel_id);
+    Boolean singEnd
+    (
+            @EDoc(doc = "机器人ID")
+            @NonNull @ENonNull String bot_id,
+
+            @EDoc(doc = "子频道ID")
+            @NonNull @ENonNull String channel_id
+    );
 }
