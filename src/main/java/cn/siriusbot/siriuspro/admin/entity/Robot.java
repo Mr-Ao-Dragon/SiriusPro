c33d38862b9901642b42999b1fc98f4e2e473b25
xziying <xziying@vip.qq.com>
2023-02-13 10:50:45 +0800

    [新增] 前端

--- src/main/java/cn/siriusbot/siriuspro/admin/entity/Robot.java
+++ src/main/java/cn/siriusbot/siriuspro/admin/entity/Robot.java
@@ -34,11 +34,11 @@ public class Robot {
     Boolean autoLogin;
 
     /**
-     * 是否在线 0 未登录 1 已登录 2 登录中 3 登录失败
+     * 是否在线 0 未登录 1 登录中 2 已登录  3 登录失败
      */
     @TableField(exist = false)
     Integer state = 0;
 
     public static final int STATE_READY = 0;    // 未登录
-    public static final int STATE_ONLINE = 1;    // 已登录
+    public static final int STATE_ONLINE = 2;    // 已登录
 }
---
+++ src/main/java/cn/siriusbot/siriuspro/config/ResourceConfig.java
@@ -0,0 +1,36 @@
+package cn.siriusbot.siriuspro.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class ResourceConfig extends WebMvcConfigurationSupport {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")    // 虚拟路径
+                // file: 表示以本地的路径方式去访问绝对路径。
+                .addResourceLocations("classpath:/static/");    // 相对路径
+        // .addResourceLocations("file:");    // 绝对路径
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry
+                .addViewController( "/" )
+                .setViewName( "forward:/index.html" );
+        registry
+                .addViewController("/user/login")
+                .setViewName( "forward:/index.html" );
+        registry
+                .addViewController("/user/login")
+                .setViewName( "forward:/index.html" );
+        registry
+                .addViewController("/qqbot")
+                .setViewName( "forward:/index.html" );
+
+    }
+}
