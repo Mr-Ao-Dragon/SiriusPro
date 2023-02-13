5020cacfe3bed1f47c11eab0c7d2f60baca69834
Mr.01010011 <37700432+SichuanMirrorCore@users.noreply.github.com>
2023-02-13 14:32:13 +0800

    Revert "[新增] 前端"
    This reverts commit c33d38862b9901642b42999b1fc98f4e2e473b25.

--- src/main/java/cn/siriusbot/siriuspro/admin/webapi/AdminController.java
+++ src/main/java/cn/siriusbot/siriuspro/admin/webapi/AdminController.java
@@ -4,9 +4,7 @@ import cn.siriusbot.siriuspro.admin.entity.Admin;
 import cn.siriusbot.siriuspro.admin.service.AdminService;
 import cn.siriusbot.siriuspro.config.Constant;
 import cn.siriusbot.siriuspro.web.R.R;
-import com.alibaba.fastjson2.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,197 +12,20 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpSession;
 
 @RestController
-@RequestMapping
+@RequestMapping("/admin")
 public class AdminController {
 
     @Autowired
     AdminService adminService;
 
-    @RequestMapping("/api/login/account")
+    @RequestMapping("/login")
     public R login(
-            @RequestBody JSONObject body,
+            @RequestParam(value = "account", required = false) String account,
+            @RequestParam(value = "passwd", required = false) String passwd,
             HttpSession session
     ) {
-        String account = body.getString("username");
-        String passwd = body.getString("password");
         Admin login = adminService.login(account, passwd);
         session.setAttribute(Constant.SESSION_ADMIN, login);
         return new R().setMsg("登录成功!");
     }
-
-
-    @RequestMapping("/api/currentUser")
-    public String currentUser(
-            HttpSession session
-    ) {
-        Admin admin = (Admin) session.getAttribute(Constant.SESSION_ADMIN);
-        if (admin == null){
-            return """
-                    {
-                      "success": false
-                    }""";
-        }
-        return """
-                {
-                  "success": true,
-                  "data": {
-                    "name": "天狼星",
-                    "avatar": "https://gw.alipayobjects.com/zos/antfincdn/XAosXuNZyF/BiazfanxmamNRoxxVxka.png",
-                    "userid": "00000001",
-                    "email": "antdesign@alipay.com",
-                    "signature": "海纳百川，有容乃大",
-                    "title": "交互专家",
-                    "group": "蚂蚁金服－某某某事业群－某某平台部－某某技术部－UED",
-                    "tags": [
-                      {
-                        "key": "0",
-                        "label": "很有想法的"
-                      },
-                      {
-                        "key": "1",
-                        "label": "专注设计"
-                      },
-                      {
-                        "key": "2",
-                        "label": "辣~"
-                      },
-                      {
-                        "key": "3",
-                        "label": "大长腿"
-                      },
-                      {
-                        "key": "4",
-                        "label": "川妹子"
-                      },
-                      {
-                        "key": "5",
-                        "label": "海纳百川"
-                      }
-                    ],
-                    "notifyCount": 12,
-                    "unreadCount": 11,
-                    "country": "China",
-                    "access": "admin",
-                    "geographic": {
-                      "province": {
-                        "label": "浙江省",
-                        "key": "330000"
-                      },
-                      "city": {
-                        "label": "杭州市",
-                        "key": "330100"
-                      }
-                    },
-                    "address": "西湖区工专路 77 号",
-                    "phone": "0752-268888888"
-                  }
-                }""";
-    }
-
-
-    @RequestMapping("/api/notices")
-    public String notices(){
-        return """
-                {
-                  "data": [
-                    {
-                      "id": "000000001",
-                      "avatar": "https://gw.alipayobjects.com/zos/rmsportal/ThXAXghbEsBCCSDihZxY.png",
-                      "title": "你收到了 14 份新周报",
-                      "datetime": "2017-08-09",
-                      "type": "notification"
-                    },
-                    {
-                      "id": "000000002",
-                      "avatar": "https://gw.alipayobjects.com/zos/rmsportal/OKJXDXrmkNshAMvwtvhu.png",
-                      "title": "你推荐的 曲妮妮 已通过第三轮面试",
-                      "datetime": "2017-08-08",
-                      "type": "notification"
-                    },
-                    {
-                      "id": "000000003",
-                      "avatar": "https://gw.alipayobjects.com/zos/rmsportal/kISTdvpyTAhtGxpovNWd.png",
-                      "title": "这种模板可以区分多种通知类型",
-                      "datetime": "2017-08-07",
-                      "read": true,
-                      "type": "notification"
-                    },
-                    {
-                      "id": "000000004",
-                      "avatar": "https://gw.alipayobjects.com/zos/rmsportal/GvqBnKhFgObvnSGkDsje.png",
-                      "title": "左侧图标用于区分不同的类型",
-                      "datetime": "2017-08-07",
-                      "type": "notification"
-                    },
-                    {
-                      "id": "000000005",
-                      "avatar": "https://gw.alipayobjects.com/zos/rmsportal/ThXAXghbEsBCCSDihZxY.png",
-                      "title": "内容不要超过两行字，超出时自动截断",
-                      "datetime": "2017-08-07",
-                      "type": "notification"
-                    },
-                    {
-                      "id": "000000006",
-                      "avatar": "https://gw.alipayobjects.com/zos/rmsportal/fcHMVNCjPOsbUGdEduuv.jpeg",
-                      "title": "曲丽丽 评论了你",
-                      "description": "描述信息描述信息描述信息",
-                      "datetime": "2017-08-07",
-                      "type": "message",
-                      "clickClose": true
-                    },
-                    {
-                      "id": "000000007",
-                      "avatar": "https://gw.alipayobjects.com/zos/rmsportal/fcHMVNCjPOsbUGdEduuv.jpeg",
-                      "title": "朱偏右 回复了你",
-                      "description": "这种模板用于提醒谁与你发生了互动，左侧放『谁』的头像",
-                      "datetime": "2017-08-07",
-                      "type": "message",
-                      "clickClose": true
-                    },
-                    {
-                      "id": "000000008",
-                      "avatar": "https://gw.alipayobjects.com/zos/rmsportal/fcHMVNCjPOsbUGdEduuv.jpeg",
-                      "title": "标题",
-                      "description": "这种模板用于提醒谁与你发生了互动，左侧放『谁』的头像",
-                      "datetime": "2017-08-07",
-                      "type": "message",
-                      "clickClose": true
-                    },
-                    {
-                      "id": "000000009",
-                      "title": "任务名称",
-                      "description": "任务需要在 2017-01-12 20:00 前启动",
-                      "extra": "未开始",
-                      "status": "todo",
-                      "type": "event"
-                    },
-                    {
-                      "id": "000000010",
-                      "title": "第三方紧急代码变更",
-                      "description": "冠霖提交于 2017-01-06，需在 2017-01-07 前完成代码变更任务",
-                      "extra": "马上到期",
-                      "status": "urgent",
-                      "type": "event"
-                    },
-                    {
-                      "id": "000000011",
-                      "title": "信息安全考试",
-                      "description": "指派竹尔于 2017-01-09 前完成更新并发布",
-                      "extra": "已耗时 8 天",
-                      "status": "doing",
-                      "type": "event"
-                    },
-                    {
-                      "id": "000000012",
-                      "title": "ABCD 版本发布",
-                      "description": "冠霖提交于 2017-01-06，需在 2017-01-07 前完成代码变更任务",
-                      "extra": "进行中",
-                      "status": "processing",
-                      "type": "event"
-                    }
-                  ]
-                }""";
-    }
-
-
 }
