0f3ba2d310085a2329c962b3c7ac1b8f3ef35db1
xziying <xziying@vip.qq.com>
2023-02-25 12:50:27 +0800

    [更新] 区分不同系统分隔符

--- src/main/java/cn/siriusbot/siriuspro/config/Constant.java
+++ src/main/java/cn/siriusbot/siriuspro/config/Constant.java
@@ -6,4 +6,20 @@ public interface Constant {
      * session-管理员对象
      */
     String SESSION_ADMIN = "SESSION_ADMIN";
+
+    /**
+     * 分隔符
+     * @return
+     */
+    default String SEPARATOR() {
+        String os = System.getProperty("os.name");
+        //Windows操作系统
+        if (os != null && os.toLowerCase().startsWith("windows")) {
+            return "\\";
+        } else if (os != null && os.toLowerCase().startsWith("linux")) {//Linux操作系统
+            return "/";
+        } else { //其它操作系统
+            return "/";
+        }
+    }
 }
