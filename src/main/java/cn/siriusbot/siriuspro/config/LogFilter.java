66ceb669d0ff6c547288d92585f041d02d95dc2f
xziying <xziying@vip.qq.com>
2023-03-09 15:09:31 +0800

    [新增] 日志前端显示

--- src/main/java/cn/siriusbot/siriuspro/config/LogFilter.java
+++ src/main/java/cn/siriusbot/siriuspro/config/LogFilter.java
@@ -2,17 +2,12 @@ package cn.siriusbot.siriuspro.config;
 
 import ch.qos.logback.classic.PatternLayout;
 import ch.qos.logback.classic.spi.ILoggingEvent;
-import ch.qos.logback.classic.spi.IThrowableProxy;
 import ch.qos.logback.core.UnsynchronizedAppenderBase;
-import ch.qos.logback.core.filter.Filter;
-import ch.qos.logback.core.spi.FilterReply;
 import cn.siriusbot.siriuspro.admin.service.LogService;
 import cn.siriusbot.siriuspro.uitls.AppContextUtil;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 
 @EqualsAndHashCode(callSuper = true)
 @Component
@@ -24,12 +19,40 @@ public class LogFilter extends UnsynchronizedAppenderBase<ILoggingEvent> {
     LogService logService;
 
 
-
     @Override
     protected void append(ILoggingEvent event) {
-        if (logService == null){
+        if (logService == null) {
             logService = AppContextUtil.getBean(LogService.class);
         }
-        logService.pushLog(layout.doLayout(event));
+        String s = layout.doLayout(event);
+        String[] split = s.split("\\{----分割线----}");
+        split[3] = split[3].substring(1, split[3].length() - 1);
+        split[1] = split[1].replaceAll(" ", "");
+        String sb = "<span class=\"colour-time\">" +
+                split[0] +
+                "</span>" +
+                ' ' +
+                "<span class=\"" +
+                (split[1].equals("INFO") ? "colour-level" : (split[1].equals("ERROR") ? "colour-level-error" : "colour-level-warning")) +
+                "\">" +
+                split[1] +
+                "</span>" +
+                ' ' +
+                "<span class=\"colour-pid\">" +
+                split[2] +
+                "</span>" +
+                ' ' +
+                '[' +
+                "<span class=\"thread-name\">" +
+                split[3] +
+                "</span>" +
+                ']' +
+                ' ' +
+                "<span class=\"colour-class\">" +
+                split[4] +
+                "</span>" +
+                " : " +
+                split[5];
+        logService.pushLog(sb);
     }
 }
